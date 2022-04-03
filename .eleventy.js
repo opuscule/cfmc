cbf60c3c1a1eb3e298e41d3575f3c03a36f410ed
Robert C. Bowman III <bowman@opuscule.com>
2022-04-02 22:38:03 -0400

    Minify HTML

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 const { DateTime } = require("luxon");
 
+const eleventyPluginFilesMinifier = require("@sherby/eleventy-plugin-files-minifier");
+
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('./src/i');
 	eleventyConfig.addPassthroughCopy('./src/js');
@@ -7,6 +9,7 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('./src/docs');
 	eleventyConfig.addPassthroughCopy('./src/admin');
 	eleventyConfig.addPassthroughCopy('./src/_redirects');
+	eleventyConfig.addPlugin(eleventyPluginFilesMinifier);
 	eleventyConfig.addFilter("postDate", (dateObj) => {
 	  return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
 	});
